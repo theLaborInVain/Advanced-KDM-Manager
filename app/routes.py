a4640f9562b707ee4fd4d8a0045b2f98cb6e1087
Timothy O'Connell <toconnell@tyrannybelle.com>
2018-12-13 11:42:31 -0600

    3.7.0

--- app/routes.py
+++ app/routes.py
@@ -10,16 +10,23 @@ import flask
 #   Landing, downtime and login routes !
 #
 
+
 @akdm.route('/', methods=['GET','POST'])
+@akdm.route('/login', methods=['GET','POST'])
 def login():
+    # uncomment the next line to disable logins
 #    return flask.render_template('login_downtime.html', meta=settings.get_meta_dict())
     form = LoginForm()
     if form.validate_on_submit():
+        akdm.logger.info("HERE")
         flask.flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return flask.redirect('/')
-    return flask.render_template('login_open.html', meta=settings.get_meta_dict(), form=form)
+        return flask.redirect('/dashboard')
+    return flask.render_template('login_open.html', api=akdm.config['api'], meta=settings.get_meta_dict(), form=form)
 
 
+@akdm.route('/dashboard', methods=['GET','POST'])
+def dashboard():
+    return flask.render_template('login_downtime.html', api=akdm.config['api'], meta=settings.get_meta_dict())
 
 #
 #   Static and media routes!
